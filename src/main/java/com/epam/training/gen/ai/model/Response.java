2367cf4079e8d4c58737c7eb67c7a748e14744d9
Bodik <fedyna.bogdan@gmail.com>
2024-11-18 17:38:47 +0200

    01-genai-basic implemented controller which allows user to interact with chatGpt

---
+++ src/main/java/com/epam/training/gen/ai/model/Response.java
@@ -0,0 +1,14 @@
+package com.epam.training.gen.ai.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Response {
+
+    private String result;
+
+}
